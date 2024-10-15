0331cfdd08fa5f167b992dbea847cd3b442e0ef3
longbowlu <zl9394@gmail.com>
2024-10-14 23:03:50 -0700

    update gas

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,10 @@ async fn main() -> anyhow::Result<()> {
     assert_eq!(address, SuiAddress::from_str("0x8f7472504821715512572f29b521d10af0b11ecd27f887f6c7a55ad020c184e7").unwrap());
     let rgp = sui_client.governance_api().get_reference_gas_price().await.unwrap();
     let mut total_rebate = 0;
+    let gas = ObjectID::from_hex_literal("0x0173200e109a96c232f34460becef9251a198b2c9bd08dfe1260ece6a90635eb").unwrap();
+    let mut gas_obj = sui_client.read_api().get_object_with_options(gas, SuiObjectDataOptions::new()).await.unwrap().data.unwrap().object_ref();
     loop {
         let stream = sui_client.coin_read_api().get_coins_stream(address, Some("0x2::sui::SUI".to_string()));
-        let gas = ObjectID::from_hex_literal("0x0173200e109a96c232f34460becef9251a198b2c9bd08dfe1260ece6a90635eb").unwrap();
-        let gas_obj = sui_client.read_api().get_object_with_options(gas, SuiObjectDataOptions::new()).await.unwrap().data.unwrap().object_ref();
         let coins = stream
             .take(512)
             .map(|coin| {
@@ -72,7 +72,9 @@ async fn main() -> anyhow::Result<()> {
         let tx = Transaction::from_data_and_signer(tx_data, vec![&key]);
         let resp = sui_client.quorum_driver_api().execute_transaction_block(tx, SuiTransactionBlockResponseOptions::new().with_effects(), None).await.unwrap();
         assert_eq!(resp.status_ok(), Some(true));
-        let rebate = resp.effects.unwrap().gas_cost_summary().net_gas_usage();
+        let effects = resp.effects.unwrap();
+        let rebate = effects.gas_cost_summary().net_gas_usage();
+        gas_obj = effects.gas_object().reference.to_object_ref();
         println!("merged {len2} coins and got back {} SUI", rebate / -1_000_000_000);
         total_rebate += rebate;
         if len1 < 512 {
